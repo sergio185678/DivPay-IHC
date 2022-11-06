4af6f46d00e8d2d4983934a2cad8ad11bbd60585
JhairQ <jaykansas7@gmail.com>
2022-11-06 16:38:16 -0500

    Realice las HU11, HU12 y HU13 que incluyen la sección de cuentas bancarias

---
+++ prototipo divpay/cuentas bancarias/delete.js
@@ -0,0 +1,15 @@
+function eliminar1(){
+    window.alert('¿Seguro que deseas eliminar esta cuenta?')
+}
+
+function eliminar2(){
+    window.alert('¿Seguro que deseas eliminar esta cuenta?')
+}
+
+function eliminar3(){
+    window.alert('¿Seguro que deseas eliminar esta cuenta?')
+}
+
+function guardarcambios(){
+    window.alert('Los cambios se realizaron de manera exitosa')
+}
