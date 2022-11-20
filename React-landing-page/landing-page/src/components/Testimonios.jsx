162c2f13ff04ebf8aa5ec37d3bd326eb7705c861
JhairQ <jaykansas7@gmail.com>
2022-11-20 18:11:50 -0500

    Ultimos cambios para el Landing Page

--- React-landing-page/landing-page/src/components/Testimonios.jsx
+++ React-landing-page/landing-page/src/components/Testimonios.jsx
@@ -6,7 +6,7 @@ function Testimonios() {
             <h2 className="titulo">Testimonios y funcionalidades de DivPay</h2>
             <p >A continuación se presentará un video sobre la explicación del propósito, beneficios, las principales caracteristicas y también testimonios de nuestra plataforma de cambios online DivPay.</p>
             <br/><br/><br/>
-            <iframe src="https://www.youtube.com/embed/Pv-ef19xObc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/sk8AYTN4kJA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </section>
   );
 }
